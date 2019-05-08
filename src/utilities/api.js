07dc8f3135dd6995098396c49fd57ed5c979c266
Adil Altaf <adil@outlook.com>
2019-05-08 12:12:36 +0500

    Added Utilities Added Avatars

---
+++ src/utilities/api.js
@@ -0,0 +1,25 @@
+import {
+    _getUsers,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer
+} from './_DATA.js';
+
+
+export function getInitialData() {
+    return Promise.all([
+        _getUsers(),
+        _getQuestions()
+    ]).then(([users,questions]) => ({
+        users,
+        questions
+    }));
+}
+
+export function addQuestion(question){
+    return _saveQuestion(question);
+}
+
+export function addQuestionAnswer(answer){
+    return _saveQuestionAnswer(answer);
+}
---
+++ src/utilities/helper.js
@@ -0,0 +1,17 @@
+export function formatQuestion (question, author, authedUser) {
+    const { id, optionOne, optionTwo, timestamp } = question
+    const { name, avatarURL } = author
+
+    return {
+      authorName: name,
+      id,
+      timestamp,
+      avatarURL: avatarURL,
+      optionOne,
+      optionTwo
+    }
+  }
+
+export function userScore(user){
+    return Object.keys(user.answers).length + user.questions.length;
+}
